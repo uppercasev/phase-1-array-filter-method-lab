1ed719881efbef917a2ead469b44ad1b80a5d55a
Vi Tran <contact.vitran@gmail.com>
2021-10-20 21:20:06 +1100

    Finished assignment

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solution here
+
+function findMatching(drivers, name) {
+    return drivers.filter(
+        function(driver) {
+            return driver.toLowerCase() === name.toLowerCase();
+        }
+    )    
+};
+
+function fuzzyMatch(drivers, letters) {
+    return drivers.filter(
+        function(driver) {
+            return driver.startsWith(letters);
+        }
+    )
+}
+
+function matchName(driverDetails, name) {
+    return driverDetails.filter(
+        function(driver){
+            return driver.name === name;
+        }
+    )
+}
